ba1490b7d343a7a205a14a5518feb46ed771a51a
Jerry Overton <jerryaoverton@yahoo.com>
2017-12-18 07:31:23 -0600

    Uses the Datamuse API to find similar words

---
+++ datamuse.R
@@ -0,0 +1,44 @@
+
+add_related_terms <- function(terms.original){
+  
+  #break text into individual terms
+  terms <- as.list(strsplit(terms.original, ",")[[1]])
+  
+  #get rid of leading and trailing spaces in terms
+  terms.trim <- trimws(terms, which = c("both", "left", "right"))
+  
+  #turn a term into a query parameter
+  parameters <-  gsub('([[:punct:]])|\\s+','+',terms.trim)
+  
+  #create api query
+  dataMuseURL <- "https://api.datamuse.com/words?ml="
+  queries <- paste(dataMuseURL,parameters, sep = "")
+  
+  related.terms.final <- terms.original
+  
+  try({
+    for (query in queries){
+      
+      library(jsonlite)
+      related.terms <- fromJSON(query)
+      
+      # sort related terms by score
+      related.terms <- related.terms[order(-related.terms$score),]
+      
+      #collapse the top N related terms into a comma seperated list
+      num_related_terms = 3
+      
+      related.terms.collapse <- related.terms[1,]$word
+      for(i in 2:num_related_terms){
+        related.terms.collapse <- paste(related.terms.collapse, related.terms[i,]$word, sep = ",")
+      }
+      
+      #add the latest set of terms to a final list
+      related.terms.final <- paste(related.terms.final, related.terms.collapse, sep = ",")
+      
+    }
+  })
+  
+  return(related.terms.final)
+  
+}
